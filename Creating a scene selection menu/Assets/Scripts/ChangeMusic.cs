34e9f5859027d11db157bad231796e7b62bdd1f3
ibmaciel <ibmaciel@users.noreply.github.com>
2018-12-13 13:24:21 -0200

    Finished

---
+++ Creating a scene selection menu/Assets/Scripts/ChangeMusic.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ChangeMusic : MonoBehaviour
+{
+    public AudioClip level2Music;
+
+    private AudioSource source;
+
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    void OnLevelWasLoaded(int level)
+    {
+        if (level == 2)
+        {
+            source.clip = level2Music;
+            source.Play();
+        }
+    }
+}
